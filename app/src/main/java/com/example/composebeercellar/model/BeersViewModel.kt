9c330bf036144af1a811225616d2db3c3ac77db3
Fr0styy <lassehindsberg@gmail.com>
2024-09-30 22:56:30 +0200

    added: add and delete functionality

--- app/src/main/java/com/example/composebeercellar/model/BeersViewModel.kt
+++ app/src/main/java/com/example/composebeercellar/model/BeersViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.composebeercellar.model
 
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
+import com.example.composebeercellar.repository.BeersRepository
 
 class BeersViewModel : ViewModel() {
+private val repository = BeersRepository()
+    val beersFlow: State<List<Beer>> = repository.BeersFlow
+    val errorMessageFlow: State<String> = repository.errorMessageFlow
 
+    val reloadingFlow: State<Boolean> = repository.isLoading
+
+    init {
+        reload()
+    }
+
+    private fun reload() {
+        repository.getBeers()
+    }
+
+    private fun add(beer: Beer) {
+        repository.add(beer)
+    }
+
+    private fun delete(beer: Beer) {
+        repository.delete(beer.id)
+    }
 }
