1c1b023b78d1a26cae4c23c50f32a7230b209af6
LasseHindsberg <lassehindsberg@gmail.com>
2024-09-30 10:56:23 +0200

    Added Retrofit REST service calls

---
+++ app/src/main/java/com/example/composebeercellar/repository/BeersService.kt
@@ -0,0 +1,31 @@
+package com.example.composebeercellar.repository
+
+// Imports
+import com.example.composebeercellar.model.Beer
+import retrofit2.Call
+import retrofit2.http.*
+
+
+interface BeersService {
+    // Get All
+    @GET("beers")
+    fun getAllBeers(): Call<List<Beer>>
+
+
+    // Get by Id
+    @GET("beers/{BeerId}")
+    fun GetBeerById(@Path("BeerId") BeerId: Int): Call<Beer>
+
+    // POST
+    @POST("beers")
+    fun saveBeer(@Body beer: Beer): Call<Beer>
+
+    // Remove
+    @DELETE("beers/{id}")
+    fun deleteBeer(@Path("id") id: Int): Call<Beer>
+
+    // UPDATE
+    @PUT("beers/{id}")
+    fun updateBeer(@Path("id") id: Int, @Body beer: Beer): Call<Beer>
+
+}
